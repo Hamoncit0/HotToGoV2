cafb3ee69f57b4c7d9bca6cc1e9f8f5cd2206a02
Hamoncit0 <151789307+Hamoncit0@users.noreply.github.com>
2024-11-23 18:13:54 -0600

    Stronger Lighting

--- client/src/scene/lighting.js
+++ client/src/scene/lighting.js
@@ -1,11 +1,11 @@
 import * as THREE from 'three';
 
 export function setupLighting(scene) {
-  const ambientLight = new THREE.AmbientLight(0xffffff, 4);
+  const ambientLight = new THREE.AmbientLight(0xffffff, 3.5);
   scene.add(ambientLight);
 
-  const pointLight = new THREE.PointLight( 0x0080d4, 4, 100 );
-  pointLight.position.set( 0, 4, -8.5 );
+  const pointLight = new THREE.PointLight( 0x0080d4, 15, 100 );
+  pointLight.position.set( 0, 3.5, -8.5 );
   scene.add( pointLight );
 
 }
@@ -14,7 +14,7 @@ export function setUpLightingBeach(scene) {
   const ambientLight = new THREE.AmbientLight(0xffffff, 6);
   scene.add(ambientLight);
 
-  const pointLight = new THREE.PointLight( 0xff2d00, 8, 100 );
+  const pointLight = new THREE.PointLight( 0xff2d00, 10, 100 );
   pointLight.position.set( 1, 4, -8.7 );
   scene.add( pointLight );
 
@@ -27,7 +27,7 @@ export function setUpLightingMinecraft(scene) {
   const ambientLight = new THREE.AmbientLight(0xffffff, 3);
   scene.add(ambientLight);
 
-  const pointLight = new THREE.PointLight( 0xffffff, 6, 100 );
+  const pointLight = new THREE.PointLight( 0xffffff, 10, 100 );
   pointLight.position.set( 0, 10, -8 );
   scene.add( pointLight );
 
